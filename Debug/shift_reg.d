63eef57b5e3557118eeb6603c09f8f0c1e9cc1e7
kyznecov <kyznecov@host023026.(none)>
2011-04-29 13:45:27 +0400

    new2

---
+++ Debug/shift_reg.d
@@ -0,0 +1,34 @@
+shift_reg.d shift_reg.o: ../shift_reg.c C:\WinAVR\avr\include/avr/io.h \
+  C:\WinAVR\avr\include/avr/sfr_defs.h C:\WinAVR\avr\include/inttypes.h \
+  C:\WinAVR\avr\include/stdint.h C:\WinAVR\avr\include/avr/iom16.h \
+  C:\WinAVR\avr\include/avr/portpins.h C:\WinAVR\avr\include/avr/common.h \
+  C:\WinAVR\avr\include/avr/version.h C:\WinAVR\avr\include/avr/fuse.h \
+  C:\WinAVR\avr\include/avr/lock.h ../shift_reg.h \
+  C:\WinAVR\avr\include/util/delay.h \
+  C:\WinAVR\avr\include/util/delay_basic.h
+
+C:\WinAVR\avr\include/avr/io.h:
+
+C:\WinAVR\avr\include/avr/sfr_defs.h:
+
+C:\WinAVR\avr\include/inttypes.h:
+
+C:\WinAVR\avr\include/stdint.h:
+
+C:\WinAVR\avr\include/avr/iom16.h:
+
+C:\WinAVR\avr\include/avr/portpins.h:
+
+C:\WinAVR\avr\include/avr/common.h:
+
+C:\WinAVR\avr\include/avr/version.h:
+
+C:\WinAVR\avr\include/avr/fuse.h:
+
+C:\WinAVR\avr\include/avr/lock.h:
+
+../shift_reg.h:
+
+C:\WinAVR\avr\include/util/delay.h:
+
+C:\WinAVR\avr\include/util/delay_basic.h:
